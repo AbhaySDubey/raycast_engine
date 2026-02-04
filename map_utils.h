15acb8eedc5dd6c2c5d96021a6fb3990df1c821c
Abhay Dubey <dabhaysuresh@gmail.com>
2026-02-04 09:19:30 +0530

    map_utils header

--- map_utils.h
+++ map_utils.h
@@ -7,15 +7,19 @@
 #include <utility>
 
 // currently only a 10x15 grid is being used
-constexpr std::pair<int, int> NTILES = { 10,15 };
+
+// ntile initialization and access
+std::pair<int, int> NTILES();
+void initNTILES(std::pair<int, int> ntiles);
+
 extern const std::vector<Color> mapColors;
 
 // functions
 std::vector<int> getSpaceSepVals(std::string &line);
 std::vector<std::vector<Color>> createMap(std::string &mapFn);
-void drawMap(std::vector<std::vector<Color>> &map, float CELL_SIZE);
-Vector2 normalize(Vector2 a);
-float vec_mag(Vector2 a);
+void drawMap(std::vector<std::vector<Color>> &map, RenderTexture2D &minimap);
+Vector2 normalize(Vector2 &a);
+float vec_mag(Vector2 &a);
 
 #endif
 
