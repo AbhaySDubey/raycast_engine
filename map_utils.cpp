2ab7efe3c348e619bcb7aa32f417b40a3af0430c
Abhay Dubey <dabhaysuresh@gmail.com>
2026-02-04 09:19:04 +0530

    refactored some code; added explicit type conversions wherever necessary; implemented dynamic map creation based on input map file

--- map_utils.cpp
+++ map_utils.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cassert>
+#include <exception>
 
 const std::vector<Color> mapColors = {
     BLANK, DARKGRAY, MAROON, ORANGE, DARKGREEN, DARKBLUE, DARKPURPLE,
@@ -13,11 +15,27 @@ const std::vector<Color> mapColors = {
     LIGHTGRAY, PINK, YELLOW, GREEN, SKYBLUE, PURPLE, BEIGE, BLACK,
 };
 
+namespace {
+    std::pair<int, int> ntiles = { 10,15 };
+    bool ntiles_init = false;
+}
+
+std::pair<int,int> NTILES() {
+    assert(ntiles_init && "Number of tiles not yet initialized!");
+    return ntiles;
+}
+
+void initNTILES(std::pair<int, int> nTiles) {
+    assert(!ntiles_init && "Number of tiles already initialized!");
+    ntiles = nTiles;
+    ntiles_init = true;
+}
+
 std::vector<int> getSpaceSepVals(std::string &line) {
     std::string buff;
     std::vector<int> vals;
     bool errCheck = false;
-    for (int i = 0; i <= line.size(); i++) {
+    for (size_t i = 0; i <= line.size(); i++) {
         auto c = (i < line.size() ? line[i] : ' ');
         if (c == ' ' && !buff.empty()) {
             try {
@@ -47,27 +65,45 @@ std::vector<int> getSpaceSepVals(std::string &line) {
 
 std::vector<std::vector<Color>> createMap(std::string &mapFn) {
     std::ifstream map1(mapFn);
+
+    if (!map1) {
+        std::cerr << "Unable to open map definition file. Please check file existence and validate that file path is correct." << std::endl;
+        return std::vector<std::vector<Color>>({});
+    }
+
     std::string line;
 
     std::vector<std::vector<int>> wallDefs;
 
+    bool setCustomNTILES = false;
+
+    std::pair<int, int> nTiles = { 10,15 };
+
     while (std::getline(map1, line)) {
         std::vector<int> vals = getSpaceSepVals(line);
         if (!vals.empty()) {
-            wallDefs.push_back(vals);
+            if (vals.size()==2 && !setCustomNTILES) {
+                nTiles = std::make_pair( vals[0],vals[1] );
+                setCustomNTILES = true;
+            }
+            else {
+                wallDefs.push_back(vals);
+            }
         }
     }
 
-    std::vector<std::vector<Color>> map(NTILES.first, std::vector<Color>(NTILES.second, mapColors[0]));
+    initNTILES(nTiles);
     
+    std::vector<std::vector<Color>> map(NTILES().first, std::vector<Color>(NTILES().second, mapColors[0]));
+
     // map boundaries
-    for (int i = 0; i < NTILES.first; i++) {
+    for (int i = 0; i < NTILES().first; i++) {
         map[i][0] = mapColors[1];
-        map[i][NTILES.second-1] = mapColors[1];
+        map[i][NTILES().second-1] = mapColors[1];
     }
-    for (int j = 0; j < NTILES.second; j++) {
+    for (int j = 0; j < NTILES().second; j++) {
         map[0][j] = mapColors[1];
-        map[NTILES.first-1][j] = mapColors[1];
+        map[NTILES().first-1][j] = mapColors[1];
     }
     // actual walls
     for (auto &it : wallDefs) {
@@ -78,29 +114,38 @@ std::vector<std::vector<Color>> createMap(std::string &mapFn) {
     return map;
 }
 
-void drawMap(std::vector<std::vector<Color>> &map, float CELL_SIZE) {
+void drawMap(std::vector<std::vector<Color>> &map, RenderTexture2D &minimap) {
     // Vector2 tilePos = { 0, 0 };
     int x = 0, y = 0;
-    for (int i = 0; i < NTILES.first; i++) {
-        for (int j = 0; j < NTILES.second; j++) {
-            DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, map[i][j]);
-            // purely for visualization
-            DrawRectangleLines(x, y, CELL_SIZE,CELL_SIZE, WHITE);
-            x += CELL_SIZE;
+    // Vector2 mm_cell_size =  { CELL_SIZE.x, CELL_SIZE.y };
+
+    Vector2 mm_cell_size = { minimap.texture.width/(float)NTILES().first, minimap.texture.height/(float)NTILES().second };
+
+    BeginTextureMode(minimap);
+        ClearBackground(BLACK);
+        for (int i = 0; i < NTILES().first; i++) {
+            for (int j = 0; j < NTILES().second; j++) {
+                DrawRectangle(x, y, mm_cell_size.x, mm_cell_size.y, map[i][j]);
+                // purely for visualization
+                DrawRectangleLines(x, y, mm_cell_size.x, mm_cell_size.y, WHITE);
+                x += mm_cell_size.x;
+            }
+            y += mm_cell_size.y;
+            x = 0;
         }
-        y += CELL_SIZE;
-        x = 0;
-    }
+    EndTextureMode();
 }
 
 
-Vector2 normalize(Vector2 a) {
-    float mag = (float)(sqrt(a.x*a.x + a.y*a.y));
-
-    return a/mag;
-}
-
-float vec_mag(Vector2 a) {
+float vec_mag(Vector2 &a) {
     float mag = (float)(std::sqrt(a.x*a.x + a.y*a.y));
     return mag;
+}
+
+Vector2 normalize(Vector2 &a) {
+    float m = vec_mag(a);
+    if (m == 0) {
+        return (Vector2){0,0};
+    }    
+    return a/m;
 }
